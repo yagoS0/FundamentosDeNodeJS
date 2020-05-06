65d7598efec99391930ce8a3779321849e3d067b
yago silva <yagoas120@gmail.com>
2020-05-06 12:09:37 -0300

    Finalizado

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -9,11 +9,10 @@ const transactionsRepository = new TransactionsRepository();
 transactionRouter.get('/', (request, response) => {
   try {
     const transactions = transactionsRepository.all();
-    const balence = transactionsRepository.getBalance();
-    return response.json({ transactions, balence });
+    const balance = transactionsRepository.getBalance();
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
-    1;
   }
 });
 
